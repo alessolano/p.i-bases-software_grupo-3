2975859cefa1d5b1cc184085f5e9de2501356719
Silvio <silvio.castilo@ucr.ac.cr>
2026-09-15 22:31:07 -0600

    added to the sql script sequences to create table of companies, theaters, employees, movie_laguages

--- database/script.sql
+++ database/script.sql
@@ -30,3 +30,62 @@ CREATE TABLE invoice_products
     product_id NUMBER NOT NULL,
     CONSTRAINT invoice_products_pk PRIMARY KEY (invoice_id, product_id),
 );
+
+CREATE TABLE companies
+(
+    company_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR(254) NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    phone NUMBER NOT NULL,
+    website_url VARCHAR(2048) NOT NULL,
+    addres_id NUMBER NOT NULL,
+    CONSTRAINT FK_address_company FOREIGN KEY (address_id) REFERENCES addresses(address_id)
+);
+
+CREATE TABLE theaters
+(
+    theater_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cinema_id NUMBER NOT NULL,
+    number_seats NUMBER NOT NULL,
+    dimension_x NUMBER,
+    dimension_y NUMBER,
+    projector_type VARCHAR(100),
+    sound_system VARCHAR(100),
+    CONSTRAINT FK_cinema_theater FOREIGN KEY (cinema_id) REFERENCES cinemas(cinema_id)
+    
+)
+
+CREATE TABLE movie_languages
+(
+    movie_id NUMBER NOT NULL,
+    language_id NUMBER NOT NULL,
+    CONSTRAINT PK_movie_languages PRIMARY KEY (movie_id, language_id)
+    CONSTRAINT FK_movlang_movID FOREIGN KEY(movie_id) REFERENCES movies(movie_id)
+    CONSTRAINT FK_movlang_langID FOREIGN KEY(language_id) REFERENCES languages(language_id)
+
+)
+
+CREATE TABLE employees
+(
+    Employe_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    second_name VARCHAR(100) NOT NULL,
+    first_name  VARCHAR(100) NOT NULL,
+    second_surname VARCHAR(100) NOT NULL,
+    birthday DATE NOT NULL,
+    phone_number NUMBER NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    addres_id NUMBER NOT NULL,
+    employe_role VARCHAR (20),
+    company_id NUMBER NOT NULL,
+    CONSTRAINT FK_address_employee FOREIGN KEY (addres_id) REFERENCES addresses(address_id)
+    CONSTRAINT FK_id_company FOREIGN KEY (company_id) REFERENCES companies(company_id)
+)
+/* googl authentication needs more research
+CREATE TABLE user_mfa (
+    user_id INT PRIMARY KEY,
+    encrypted_secret_key VARCHAR(255) NOT NULL,
+    is_mfa_enabled BOOLEAN DEFAULT FALSE,
+    backup_codes TEXT -- Encrypted list of emergency recovery codes
+);
+*/
