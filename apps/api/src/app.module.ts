1aff91b07bdd7b29f75acd0fa53e7fab9df9d33d
Diego <diego.bolanosvillalobos@ucr.ac.cr>
2026-09-23 13:16:23 -0600

    feat: Added health repository

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, DatabaseModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import oracle from 'oracledb';
+import path from 'node:path';
+
+oracle.initOracleClient({
+  configDir: path.resolve(__dirname, '../Wallet'),
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
