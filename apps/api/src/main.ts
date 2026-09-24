0faa6ed0f97ce112d27ace2bdd4d1d62eceb04db
Diego <diego.bolanosvillalobos@ucr.ac.cr>
2026-09-23 18:26:14 -0600

    fix: use env variable for macos support

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,9 +3,21 @@ import { AppModule } from './app.module';
 import oracle from 'oracledb';
 import path from 'node:path';
 
-oracle.initOracleClient({
+const initOptions: oracle.InitialiseOptions = {
   configDir: path.resolve(__dirname, '../Wallet'),
-});
+};
+
+if (!process.env.ORACLE_CLIENT_LIB_DIR) {
+  initOptions.libDir = process.env.ORACLE_CLIENT_LIB_DIR;
+}
+
+try {
+  oracle.initOracleClient(initOptions);
+  console.log('Oracle client initialized successfully.');
+} catch (error) {
+  console.error('Error initializing Oracle client:', error);
+  process.exit(1);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
