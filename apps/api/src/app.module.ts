75f8eb69e35269bb4a869658fd4c6359cb1936aa
Diego <diego.bolanosvillalobos@ucr.ac.cr>
2026-09-25 14:53:36 -0600

    feat: added my movie module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
   imports: [HealthModule, DatabaseModule, ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: '.env',
-  })],
+  }), MovieModule],
 })
 export class AppModule {}
